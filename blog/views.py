8fe43a76131c0a9a5a3ac31d0f4800d74b6c1d0d
MeilinLu <meilinlu86@gmail.com>
2020-01-26 11:17:56 -0500

    backend update, setting, routes, index view method

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,17 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import Http404
+from .model import Post, Comment
+from datetime import datetime
+from .forms import commentForm
 
 # Create your views here.
+def index(request):
+    try:
+        posts = Post.objects.all()
+    except Post.DoesNotExist:
+        raise Http404("Post does not exist")
+    return renden(request, 'blog/post_list.html', {'posts': posts})
+
