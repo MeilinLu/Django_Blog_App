75e4640052c7767d02a2a7e4bb6022ef97b982dd
MeilinLu <meilinlu86@gmail.com>
2020-01-26 16:38:54 -0500

    debug: create urls.py @ blog folder to store all url routes

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,10 +17,5 @@ from django.conf.urls import url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^', views.index, name='index'),
-    url(r'posts/$', views.post_list, name='post_list'),
-    url(r'posts/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
-    url(r'posts/(?P<pk>[0-9]+)/comment/$', views.post_comment, name='post_comment'),
-    url(r'posts/(?P<pk>[0-9]+)/comment/add_comment/$', views.add_comment, name='add_comment'),
-    url(r'^admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls)
 ]
