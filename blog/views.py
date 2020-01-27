9cd9cb7844c45a5f01c5f0f5a089102a9fd89551
MeilinLu <meilinlu86@gmail.com>
2020-01-26 19:18:54 -0500

    render index page

--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,22 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+#from __future__ import unicode_literals
 
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import Http404
-from .model import Post, Comment
+#from .model import Post, Comment
 from datetime import datetime
-from .forms import commentForm
+#from .forms import commentForm
 
 # Create your views here.
 def index(request):
+    date = datetime.now().strftime('%Y-%m-%d')
+    return render(request, 'index.html', {'date': date})
+'''
+def post_list(request):
     try:
         posts = Post.objects.all()
     except Post.DoesNotExist:
         raise Http404("Post does not exist")
     return renden(request, 'blog/post_list.html', {'posts': posts})
 
+'''
